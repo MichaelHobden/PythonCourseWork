2273ad7f53276f3c70bef15fc56376be2bcbd4e5
MichaelHobden <140603254+MichaelHobden@users.noreply.github.com>
2023-09-07 09:43:26 +0100

    Create LogicalOperators.py
    Second z2m excercise.  Created a simple program that prints certain messages depending on whether certain values are True or False.

---
+++ LogicalOperators.py
@@ -0,0 +1,11 @@
+is_magician = True
+is_expert = False
+
+if is_magician and is_expert:
+  print("You are a master magician")
+
+if is_magician and not is_expert:
+  print("At least you\'re getting there")
+
+if not is_magician:
+  print("You need magical powers")
